7b26dddaf3a3b9a2ca7af3d2ea2caa41d34f7de0
FikerBiruk <fikerfuffa@gmail.com>
2026-05-23 23:08:59 -0400

    added a magnetometer debug code cause PID was returning weird values

---
+++ test_mag.py
@@ -0,0 +1,13 @@
+from blimputils import Magnetometer
+import time, math
+
+mag = Magnetometer()
+mag.init()
+
+print("Move the board. Ctrl+C to stop.\n")
+
+while True:
+    mx, my, mz = mag.get_xyz()
+    mag_norm = math.sqrt(mx*mx + my*my + mz*mz)
+    print(f"MX={mx:7.2f}  MY={my:7.2f}  MZ={mz:7.2f}  |B|={mag_norm:7.2f}")
+    time.sleep(0.2)
